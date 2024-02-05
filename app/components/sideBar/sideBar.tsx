0f983fdd52542bbd21cd1be287012df0ada175e9
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2024-02-05 18:17:08 +0900

    디자인 레이아웃 작업중

--- app/components/sideBar/sideBar.tsx
+++ app/components/sideBar/sideBar.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className="fixed left-0 top-0 flex h-full flex-col items-center bg-white px-5 pt-32">
-      <Link href="/">
-        <div className="my-4 w-full rounded-xl px-3 py-2 text-center transition-all delay-100 ease-in-out hover:bg-blue-400 hover:text-white">
+    <div className="fixed left-0 top-0 hidden h-full w-44 flex-col items-center bg-white px-5 pt-32 lg:flex">
+      <Link href="/" className="my-4 w-full">
+        <div className="w-full rounded-xl px-3 py-2 text-center transition-all delay-100 ease-in-out hover:bg-blue-400 hover:text-white">
           등급 업
         </div>
       </Link>
-      <Link href="/choose">
-        <div className="my-4 w-full rounded-xl px-3 py-2 text-center transition-all delay-100 ease-in-out hover:bg-blue-400 hover:text-white">
+      <Link href="/choose" className="my-4 w-full">
+        <div className="w-full rounded-xl px-3 py-2 text-center transition-all delay-100 ease-in-out hover:bg-blue-400 hover:text-white">
           잠재능력 뽑기
         </div>
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { connectDB } from './utils/database'
-import { Button, Checkbox, FormControlLabel } from '@mui/material'
+import { Button, Checkbox, FormControlLabel, InputAdornment, TextField } from '@mui/material'
 import { equipSelect, potenSelect } from './constant'
 
 import UpResult from './components/result/upResult'
@@ -17,10 +17,10 @@ export default async function Home() {
     <main className="flex w-full flex-col justify-center bg-gray-200">
       <Header />
       <SideBar />
-      <div className="mx-auto mb-4 mt-24 w-[90vw] text-left text-3xl text-gray-600 sm:w-[600px]">
+      <div className="mx-auto mb-4 mt-24 w-[90vw] text-left text-3xl text-gray-600 sm:w-[70vw]">
         잠재능력 시뮬레이터
       </div>
-      <div className="mx-auto mb-4 w-[90vw] rounded-lg bg-white px-4 py-4 sm:w-[600px]">
+      <div className="mx-auto mb-4 w-[90vw] rounded-lg bg-white px-4 py-4 sm:w-[70vw]">
         <div className="ml-2 pb-2 text-xl text-blue-400">장비 정보</div>
         <div className="flex flex-col sm:flex-row">
           <PotenSelect title="현재 잠재능력" value={potenSelect} />
@@ -31,6 +31,14 @@ export default async function Home() {
           <PotenSelect title="현재 잠재능력" value={potenSelect} />
           <FormControlLabel control={<Checkbox />} label="미라클 타임" sx={{ marginBottom: 3, marginLeft: 1 }} />
         </div>
+        <div className="ml-2 mt-6 pb-2 text-xl text-blue-400">보유 메소 (선택)</div>
+        <TextField
+          autoComplete="off"
+          label="보유 메소"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">메소</InputAdornment>,
+          }}
+        />
         <button className="mt-5 w-[100%] bg-blue-400 py-2 text-white">계산하기</button>
       </div>
       <UpResult />
